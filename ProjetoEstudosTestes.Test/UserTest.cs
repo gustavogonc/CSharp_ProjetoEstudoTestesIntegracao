448664020ddcafac65dfd903c4bf59336246b94b
Gustavo Gonçalves <gustavoantonio01@hotmail.com>
2024-08-11 15:27:31 -0300

    Testes unitários montando via inline data

--- ProjetoEstudosTestes.Test/UserTest.cs
+++ ProjetoEstudosTestes.Test/UserTest.cs
@@ -4,6 +4,18 @@ namespace ProjetoEstudosTestes.Test
 {
     public class UserTest
     {
+        [Theory]
+        [InlineData("Teste Gutavo HardCoded", "1234566667789")]
+        [InlineData("Teste HardCoded", "6667789")]
+        [InlineData("Gustavo Gonçalves Dev", "9871134")]
+        public void GivenAnRandomUserWhenCorrectParamsUserShouldBeValid(string name, string password)
+        {
+            //Arrange 
+            var user = new Users(name, password, Guid.NewGuid(), "", "");
+
+            //Act + Assert
+            Assert.True(user.IsValid);
+        }
 
         [Fact]
         public void GivenAnValidUserWhenCorrectParamsUserShouldBeValid()
