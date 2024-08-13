13008604668db2ceab088fb83e76efe03ff8a0f7
Gustavo Gonçalves <gustavoantonio01@hotmail.com>
2024-08-13 19:26:01 -0300

    Criado teste de validação

---
+++ ProjetoEstudosTestes.Test/BooksTest.cs
@@ -0,0 +1,17 @@
+﻿using ProjetoEstudoTestes.Domain;
+
+namespace ProjetoEstudosTestes.Test
+{
+    public class BooksTest
+    {
+        [Fact]
+        public void GivenAnInvalidBook_WhenIncorrectParams_BookShouldNotBeValid()
+        {
+            //Arrange
+            var book = new Books("Título de Livro Teste", "Teste", Guid.NewGuid(), "1550300", 2024, 0);
+
+            //Act + Assert
+            Assert.False(book.IsValid);
+        }
+    }
+}
