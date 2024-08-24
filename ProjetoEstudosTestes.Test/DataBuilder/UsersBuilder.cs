f5d5f0f938f33b95b1584e563a4211e31bc39dbf
Gustavo Gonçalves <gustavoantonio01@hotmail.com>
2024-08-23 21:38:22 -0300

    Alterado a geração de dados fake

--- ProjetoEstudosTestes.Test/DataBuilder/UsersBuilder.cs
+++ ProjetoEstudosTestes.Test/DataBuilder/UsersBuilder.cs
@@ -8,9 +8,9 @@ namespace ProjetoEstudoTestes.Testes.Test.DataBuilder
 
         public Users Build()
         {
-            Users u = new Faker<Users>()
+            Users u = new Faker<Users>("pt_BR")
                 .RuleFor(c => c.Email, f => f.Person.Email)
-                .RuleFor(c => c.Password, f => f.Random.Words(1))
+                .RuleFor(c => c.Password, f => f.Internet.Password())
                 .RuleFor(c => c.Name, f => f.Person.FullName)
                 .RuleFor(c => c.CreatedOn, f => f.Date.Recent(10))
                 .RuleFor(c => c.EditedOn, f => f.Date.Recent(10))
